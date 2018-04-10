5558d32398a311a0cf5843ef18aaa77123316407
DESKTOP-VIV0S9I\Tomohiko <tomo-toyama@ll.em-net.ne.jp>
2018-04-10 23:51:21 +0900

    シーンの設定と遷移処理の追加

--- Assets/Script/GameStateManager.cs
+++ Assets/Script/GameStateManager.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    //シーン遷移処理
+    void ChangeScene() { 
+
+
+    }
 }
