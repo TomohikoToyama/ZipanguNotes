e56312505a87ac20839593304f3382068e01130e
DESKTOP-VIV0S9I\Tomohiko <tomo-toyama@ll.em-net.ne.jp>
2018-04-16 00:06:13 +0900

    遷移仮実装

--- Assets/Script/GameStateManager.cs
+++ Assets/Script/GameStateManager.cs
@@ -5,7 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour {
 
+    float distance = 200;
+    [SerializeField]
     string currentScene = "Title"; //現在のシーン.
+    string objectName =""; //タップしたオブジェクト名.
     
 
     // Use this for initialization
@@ -15,14 +18,52 @@ public class GameStateManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
+        DontDestroyOnLoad(this);
     }
 
-    //シーン遷移処理
-    public void ChangeScene() { 
+    //シーン遷移処理(仮)
+    public void ChangeScene() {
 
-        if( currentScene == "Title")
+        //タイトル画面でボタン以外タップした場合
+        if (currentScene == "Title")
+            currentScene = "Home";
             SceneManager.LoadScene("Home");
+
+        if( currentScene == "Home")
+        {
+            /*
+             * 
+             * 
+             *
+             */
+
+            
+            // タップを取得
+            if (Input.GetMouseButtonDown(0))
+            {
+                // タップ座標をrayに変換
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                // Rayの当たったオブジェクトの情報を格納する
+                RaycastHit hit = new RaycastHit();
+                // オブジェクトにrayが当たった時
+                if (Physics.Raycast(ray, out hit, distance))
+                {
+                    // rayが当たったオブジェクトの名前を取得
+                    objectName = hit.collider.gameObject.name;
+                    Debug.Log(objectName);
+                }
+
+                if(objectName == "Home")
+
+                if (objectName == "Live")
+                        SceneManager.LoadScene("Live");
+
+                if (objectName == "Other")
+                    SceneManager.LoadScene("Other");
+
+
+            }
+        }
 
     }
 }
